20243f04d0e2b0d59f9a1ca972b39a42576da888
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-28 22:18:04 +0600

    initial setup

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold text-center text-emerald-500">
+                Hi there home
+            </h2>
+            <button className="btn w-64 rounded-full">Button</button>
+        </div>
+    );
+};
+
+export default Home;
